2d5fbc1fad5cf0eeb785cb5dac79ad7efadf4ff9
Flaviu-Petre <flaviu.petre09@gmail.com>
2025-09-07 22:03:15 +0300

    [update] TrafficLight class implementation

--- Java_Design_Pattern-Behavioral_Design_Pattern-Exercise/src/main/java/com/example/StatePattern/ContextClass/TrafficLight.java
+++ Java_Design_Pattern-Behavioral_Design_Pattern-Exercise/src/main/java/com/example/StatePattern/ContextClass/TrafficLight.java
@@ -1,23 +1,48 @@
 package com.example.StatePattern.ContextClass;
 
+import com.example.StatePattern.ConcreteClass.GreenLightState;
+import com.example.StatePattern.ConcreteClass.RedLightState;
+import com.example.StatePattern.ConcreteClass.YellowLightState;
 import com.example.StatePattern.Interface.TrafficLightState;
 
 public class TrafficLight {
-    private TrafficLightState state;
+    private TrafficLightState currentState;
 
-    public void setState(TrafficLightState state) {
-        this.state = state;
+    public TrafficLight() {
+        this.currentState = new RedLightState();
     }
 
     public TrafficLightState getState() {
-        return state;
+        return currentState;
     }
 
-    public void performStateTransition(){
-        state.transition();
+    public String getCurrentStateName() {
+        return currentState.getClass().getSimpleName();
     }
 
-    public void displayCurrentState(){
-        state.display();
+    public void nextState() {
+        System.out.println("Current state: " + getCurrentStateName());
+
+        currentState.transition();
+
+        if (currentState instanceof RedLightState) {
+            this.currentState = new GreenLightState();
+        } else if (currentState instanceof GreenLightState) {
+            this.currentState = new YellowLightState();
+        } else if (currentState instanceof YellowLightState) {
+            this.currentState = new RedLightState();
+        }
+
+        System.out.println("New state: " + getCurrentStateName());
+    }
+
+    public void displayCurrentState() {
+        currentState.display();
+    }
+
+    public void operate() {
+        displayCurrentState();
+        nextState();
+        System.out.println(); // Add spacing for readability
     }
 }
