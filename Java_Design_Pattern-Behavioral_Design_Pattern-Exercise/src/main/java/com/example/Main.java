4843ae3976ff57779c7d4801102d2a24a5ceb0bf
Flaviu-Petre <flaviu.petre09@gmail.com>
2025-09-07 12:43:30 +0300

    [update] Main class with method for testing the chain of responsibility pattern and screenshot of the console output

--- Java_Design_Pattern-Behavioral_Design_Pattern-Exercise/src/main/java/com/example/Main.java
+++ Java_Design_Pattern-Behavioral_Design_Pattern-Exercise/src/main/java/com/example/Main.java
@@ -1,17 +1,57 @@
 package com.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.example.ChainofResponsibilityPattern.ComplexityLevel;
+import com.example.ChainofResponsibilityPattern.ConcreteClasses.AdvancedSupportHandler;
+import com.example.ChainofResponsibilityPattern.ConcreteClasses.BasicSupportHandler;
+import com.example.ChainofResponsibilityPattern.ConcreteClasses.IntermediateSupportHandler;
+import com.example.ChainofResponsibilityPattern.Interface.SupportHandler;
+import com.example.ChainofResponsibilityPattern.SupportTicket;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ChainOfResponsibilityPattern();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    private static void ChainOfResponsibilityPattern() {
+        System.out.println("=== Chain of Responsibility Pattern Test ===\n");
+
+        SupportHandler basicHandler = new BasicSupportHandler();
+        SupportHandler intermediateHandler = new IntermediateSupportHandler();
+        SupportHandler advancedHandler = new AdvancedSupportHandler();
+
+        basicHandler.setNextHandler(intermediateHandler);
+        intermediateHandler.setNextHandler(advancedHandler);
+
+        SupportTicket basicTicket = new SupportTicket(
+                ComplexityLevel.Basic,
+                "Password reset request"
+        );
+
+        SupportTicket intermediateTicket = new SupportTicket(
+                ComplexityLevel.Intermediate,
+                "Software installation issue"
+        );
+
+        SupportTicket advancedTicket = new SupportTicket(
+                ComplexityLevel.Advanced,
+                "Database corruption and recovery"
+        );
+
+        System.out.println("Processing tickets through the support chain:\n");
+
+        System.out.println("1. Processing Basic Ticket:");
+        System.out.println("Issue: " + basicTicket.getIssueDescription());
+        basicHandler.handleRequest(basicTicket);
+        System.out.println();
+
+        System.out.println("2. Processing Intermediate Ticket:");
+        System.out.println("Issue: " + intermediateTicket.getIssueDescription());
+        basicHandler.handleRequest(intermediateTicket);
+        System.out.println();
+
+        System.out.println("3. Processing Advanced Ticket:");
+        System.out.println("Issue: " + advancedTicket.getIssueDescription());
+        basicHandler.handleRequest(advancedTicket);
+        System.out.println();
     }
 }
