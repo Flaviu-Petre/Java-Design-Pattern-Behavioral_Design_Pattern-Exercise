2c781a9e080e9dc26865ee2b9b74df251fa02676
Flaviu-Petre <flaviu.petre09@gmail.com>
2025-09-08 11:38:32 +0300

    [added] ShoppingCart class that represents the payment processing system and holds a reference to the current payment strategy

---
+++ Java_Design_Pattern-Behavioral_Design_Pattern-Exercise/src/main/java/com/example/StrategyPattern/ShoppingCart.java
@@ -0,0 +1,13 @@
+package com.example.StrategyPattern;
+
+public class ShoppingCart {
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void checkout(double amount) {
+        paymentStrategy.processPayment(amount);
+    }
+}
